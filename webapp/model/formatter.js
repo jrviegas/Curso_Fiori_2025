ceb8aa294d6368572840ca317c329e6fd78c9f7b
José Luiz <jrviegas@hotmail.com>
2025-10-24 14:37:16 -0300

    Alteração

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -7,6 +7,46 @@ sap.ui.define([
         pattern: "dd/MM/yyyy"
     });
 
+    function resolveDate(value) {
+        if (!value) {
+            return null;
+        }
+
+        if (value instanceof Date) {
+            return value;
+        }
+
+        if (typeof value === "number") {
+            var oFromNumber = new Date(value);
+            return isNaN(oFromNumber.getTime()) ? null : oFromNumber;
+        }
+
+        if (typeof value === "string") {
+            var sTrimmed = value.trim();
+
+            if (!sTrimmed) {
+                return null;
+            }
+
+            var aMatches = /Date\(([-+]?\d+)(?:[-+]\d{4})?\)/.exec(sTrimmed);
+            if (aMatches && aMatches[1]) {
+                var iMillis = parseInt(aMatches[1], 10);
+                var oFromMatch = new Date(iMillis);
+                return isNaN(oFromMatch.getTime()) ? null : oFromMatch;
+            }
+
+            var oFromString = new Date(sTrimmed);
+            return isNaN(oFromString.getTime()) ? null : oFromString;
+        }
+
+        if (typeof value === "object" && typeof value.ms === "number") {
+            var oFromMsProperty = new Date(value.ms);
+            return isNaN(oFromMsProperty.getTime()) ? null : oFromMsProperty;
+        }
+
+        return null;
+    }
+
     return {
         formatCurrency: function (value) {
             if (value === undefined || value === null || value === "") {
@@ -25,24 +65,13 @@ sap.ui.define([
         },
 
         formatDate: function (value) {
-            if (!value) {
+            var oDate = resolveDate(value);
+
+            if (!oDate) {
                 return "";
             }
 
-            var oDate;
-            if (value instanceof Date) {
-                oDate = value;
-            } else if (typeof value === 'string') {
-                oDate = new Date(value);
-            } else {
-                return value;
-            }
-
-            if (isNaN(oDate.getTime())) {
-                return value;
-            }
-
-            return oDateFormat.format(new Date(oDate));
+            return oDateFormat.format(oDate);
         }
     };
 });
